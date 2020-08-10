d5fb75e9eae8429263410fc0e0f745ab80f3baa0
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-08-10 23:18:47 +0400

    add few comments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// * Видео для сайта
 var instance = new vidbg('.video', {
     mp4: 'video/world.mp4', // URL or relative path to MP4 video
     webm: 'video/world.webm', // URL or relative path to webm video
@@ -7,4 +8,5 @@ var instance = new vidbg('.video', {
     overlay: false, // Boolean to display the overlay or not
 });
 
+// * Паралакс эффект 
 var rellax = new Rellax('.rocket');
