1733f3ec448693bf7bf91732192662418579cad7
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-08-13 15:57:16 +0400

    bg

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-'use strict'
+"use strict";
 
 // * Видео для сайта
-var instance = new vidbg('.video', {
-    mp4: 'video/world.mp4', // URL or relative path to MP4 video
-    webm: 'video/world.webm', // URL or relative path to webm video
-    poster: 'video/poster.jpg', // URL or relative path to fallback image
-    overlay: false, // Boolean to display the overlay or not
+var instance = new vidbg(".video", {
+  mp4: "video/world.mp4", // URL or relative path to MP4 video
+  webm: "video/world.webm", // URL or relative path to webm video
+  poster: "video/poster.jpg", // URL or relative path to fallback image
+  overlay: false, // Boolean to display the overlay or not
 });
 
-// * Паралакс эффект 
-var rellax = new Rellax('.rocket');
+// * Паралакс эффект
+var rellax = new Rellax(".rocket");
